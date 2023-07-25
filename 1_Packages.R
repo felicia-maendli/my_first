367d30ef027b8ad247047272edaa3c46d71e4289
felicia-maendli <felicia.maendli@hotmail.com>
2023-07-25 15:31:48 +0200

    Create 1_Packages.R
    Erste version

---
+++ 1_Packages.R
@@ -0,0 +1,46 @@
+
+##### installierte Packages ############################
+
+###.libPaths muss ich noch in den "global options" definieren, damit standardmässig darauf zugreift. dann muss nicht hier stehen.
+.libPaths("P:/300_Grundlagen_und_staedtische_Strategien/Spezialordner_BVB_R/library")
+
+
+# packages
+# Könnte ich noch auseinandernehmen nach Thema
+library(ggplot2)
+library(ggforce)
+library(gridExtra)
+library(grid)
+library(xlsx)
+library(rstudioapi)
+library(stringi)
+library(stringr)
+library(tidyverse)
+library(readxl)
+library(srvyr)
+library(dplyr)
+library(tidyr)
+library(utils)
+library(haven)
+library(readr)
+library(purrr)
+library(gmodels)
+library(zueridown)
+library(httr)
+library(seecolor)
+
+# Karten Grafiken
+library(broom)
+library(rgdal)
+library(sf)
+library(classInt)
+
+#Grafik machen
+library(ggrepel)
+library(scales)
+library(gridExtra)
+library(grid)
+library(lattice)
+
+# R Hilfe
+library(usethis)
